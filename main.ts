c5c6ce5ce66ab9fb658450bc6682836640aa993a
nono38 <74985866+nono38@users.noreply.github.com>
2022-02-06 22:33:01 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,44 @@
+input.onButtonPressed(Button.A, function () {
+    led.unplot(perso, 4)
+    perso = perso - 1
+    led.plot(perso, 4)
+})
+input.onButtonPressed(Button.B, function () {
+    led.unplot(perso, 4)
+    perso = perso + 1
+    led.plot(perso, 4)
+})
+let perso = 0
+let mechant = -1
+perso = 2
+led.plot(perso, 4)
+basic.forever(function () {
+    for (let index = 0; index < 6; index++) {
+        basic.pause(1000)
+        led.unplot(2, mechant)
+        mechant = mechant + 1
+        led.plot(2, mechant)
+    }
+    for (let index = 0; index < 6; index++) {
+        basic.pause(1000)
+        led.unplot(2, mechant)
+        mechant = mechant + 1
+        led.plot(4, mechant)
+    }
+    for (let index = 0; index < 6; index++) {
+        basic.pause(1000)
+        led.unplot(4, mechant)
+        mechant = mechant + 1
+        led.plot(2, mechant)
+    }
+    for (let index = 0; index < 6; index++) {
+        basic.pause(1000)
+        led.unplot(2, mechant)
+        mechant = mechant + 1
+        led.plot(1, mechant)
+    }
+    led.unplot(1, mechant)
+})
+basic.forever(function () {
+	
+})
